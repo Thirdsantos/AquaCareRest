3335f591f98976ae546d69aba97719171dd54b0e
Alfredo Santos <thirdsantos666@gmail.com>
2025-04-11 07:29:07 +0800

    testing notif

--- app.py
+++ app.py
@@ -10,11 +10,9 @@ load_dotenv()
 
 app = Flask(__name__)
 
-# Enable CORS for all routes and all origins
-CORS(app, resources={r"/sensors": {"origins": "*"}})  # Allow all origins for /sensors route
 
-# Firebase credentials and initialization
-firebase_credentials = os.getenv("GOOGLE_APPLICATION_CREDENTIALS_JSON")
+CORS(app, resources={r"/sensors": {"origins": "*"}})  
+
 if firebase_credentials:
     creds = json.loads(firebase_credentials)
     if not firebase_admin._apps:
@@ -27,32 +25,35 @@ else:
     print("Error: GOOGLE_APPLICATION_CREDENTIALS_JSON not found in environment variables.")
     exit()
 
-# Firebase Realtime Database references
-ref = db.reference("Sensors")
 
-# Route to receive sensor data and update database without threshold checks
+ref = db.reference("Sensors")
+PhAlert = db.reference("Notifications/PH")
+TempAlert = db.reference("Notifications/Temperature")
+TurbAlert = db.reference("Notifications/Turbidity")
+
+
 @app.route("/sensors", methods=["POST"])
 def handle_sensors():
     try:
         data = request.json
         
-        # Check if all required keys are present
+      
         if "PH" not in data or "Temperature" not in data or "Turbidity" not in data:
             return jsonify({"error": "Missing required data fields"}), 400
         
-        # Get the sensor values
+       
         ph_value = data["PH"]
         temp_value = data["Temperature"]
         turb_value = data["Turbidity"]
         
-        # Directly update the Firebase Realtime Database with received values
+       
         ref.update({
             "PH": ph_value,
             "Temperature": temp_value,
             "Turbidity": turb_value
         })
 
-        return jsonify({"status": "Data processed and updated successfully."}), 200
+        return jsonify({TurbAlert : "Data processed and updated successfully."}), 200
     
     except Exception as e:
         print(f"Error: {e}")
